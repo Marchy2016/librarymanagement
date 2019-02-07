49adf5b6db88df17d07b606264779616d13e162c
marcus <marcus.tema@jumpco.io>
2019-02-07 09:46:59 +0200

    application run changed to default

--- src/main/java/com/roma/librarymanagment/LibrarymanagmentsystemApplication.java
+++ src/main/java/com/roma/librarymanagment/LibrarymanagmentsystemApplication.java
@@ -1,6 +1,5 @@
 package com.roma.librarymanagment;
 
-import com.roma.librarymanagment.bootstrap.DevBootStrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibrarymanagmentsystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DevBootStrap.class, args);
+        SpringApplication.run(LibrarymanagmentsystemApplication.class, args);
     }
 
 }
